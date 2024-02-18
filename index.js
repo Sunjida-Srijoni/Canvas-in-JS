71f20c964e3043c10c7f07e5285d6f2869ec19ec
Sunjida-Srijoni <sunjidasrijoni@gmail.com>
2024-02-18 23:46:22 +0600

    Creating a rectangle with color and border is completed

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var c =document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-ctx.fillRect(10,10,380,280);
+ctx.strokeStyle = "black";
+ctx.strokeRect(10,10,500,250);
+
+ctx.fillStyle = "green";
+ctx.fillRect(12,12,496,246);
+
