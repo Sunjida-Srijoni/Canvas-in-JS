bec4633819a02b62ca1a3e930fac854bed04e6d9
Sunjida-Srijoni <sunjidasrijoni@gmail.com>
2024-02-18 23:57:06 +0600

    Creating national flag of bangladesh with red circle

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var c =document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
+ctx.lineWidth=3;
 ctx.strokeStyle = "black";
-ctx.strokeRect(10,10,500,250);
+ctx.strokeRect(10,10,489,239);
 
 ctx.fillStyle = "green";
-ctx.fillRect(12,12,496,246);
+ctx.fillRect(12,12,486,236);
+
+var centerX = c.width / 2;
+var centerY = c.height / 2;
+
+console.log("X:" + centerX);
+console.log("Y:" + centerY);
+
+
+ctx.beginPath();
+ctx.arc(centerX-30,centerY,60,0,2*Math.PI,false);
+ctx.fillStyle = "red";
+ctx.fill();
+
 
